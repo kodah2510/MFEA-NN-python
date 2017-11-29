3bab6e7ba1674af62d6809b9bd249ad5a1f015c3
kodag2510 <mrkhoa1996@gmail.com>
2017-11-29 23:37:44 +0700

    update generator

--- generator.py
+++ generator.py
@@ -2,16 +2,34 @@ import numpy as np
 def generateTestSet(N):
     dim = [int(2**N), int(N)]
     sample = np.random.rand(dim[0], dim[1])
-    X = np.zeros(dim, dtype=bool)
+    X = np.zeros(dim, dtype=float)
     Y = np.empty([1, dim[0]], dtype=bool)
 
     for i in range(0, len(X)):
         increment(X[i], i + 1)
         #print(X[i].astype(float))
     
-    #X = X.T
+
+    #print(X)
+    sub_arrs = {}
+    for i in range(0, N + 1):
+        sub_arrs[i] = []
+    for x in X:
+        count = np.count_nonzero(x)
+        sub_arrs[count].append(x)
+    for i in sub_arrs:
+        np.random.shuffle(sub_arrs[i])
+
+    another_X = []
+    for i in sub_arrs:
+        for j in sub_arrs[i]:
+            another_X.append(j)
+    X = np.array(another_X)
+    #print(X)
+    #print(sub_arrs)
     
-    np.random.shuffle(X)
+    #print(sub_arrs.astype(float))
+    #np.random.shuffle(X)
     X = X.T
     for j in range(0, dim[0]):
         count = np.count_nonzero(X[::1, j])
@@ -38,7 +56,7 @@ def increment(X, times):
         cur_times += 1
     
 def main():
-    N = 5
+    N = 4
     generateTestSet(N)
     pass
 if __name__ == '__main__':
